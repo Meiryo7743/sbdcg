be5ac8399788cc3e794f13127e10b96443d0bb14
Meiryo7743 <46659204+Meiryo7743@users.noreply.github.com>
2024-04-12 10:38:31 +0900

    Add support for JSON export (experimental)

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # SIT Bus Data CSV Generator (SBDCG)
 
-芝浦工業大学の[学バス時刻表データ](http://bus.shibaura-it.ac.jp/db/bus_data.json)を CSV ファイルに変換するツールです。
+芝浦工業大学の[学バス時刻表データ](http://bus.shibaura-it.ac.jp/db/bus_data.json)を CSV/JSON ファイルに変換するツールです。
 
 ## 使い方
 
@@ -18,10 +18,18 @@
 deno task csv
 ```
 
+CSV ファイルに加えて JSON ファイルも出力したい場合は，次のコマンドを実行してください。`csv` フォルダーの直下に `busData.json` が生成されます。
+
+```
+deno task json
+```
+
 ### (2) 実行ファイルを利用する場合
 
 > [!NOTE]
 > 現在は Windows 用の `exe` ファイルのみを提供しています。
+>
+> また，CSV ファイルとして出力することしかできず，JSON ファイルでの出力には今のところ対応していません。
 
 [Releases](https://github.com/Meiryo7743/sbdcg/releases/latest/) のところから，`sbdcg_windows_x86_64.exe` をダウンロードしてください。その実行ファイルをダブルクリックすると，`csv` フォルダーの中に各種 CSV ファイルが生成されます。
 
---
+++ src/generate_json.ts
@@ -0,0 +1,96 @@
+import { join } from "https://deno.land/std@0.222.1/path/mod.ts";
+
+const generateCalendar = () => {
+  const calendar: { [key: string]: string | null } = {};
+  Deno.readTextFileSync("./csv/calendar.csv")
+    .split("\n")
+    .map((item) => item.split(","))
+    .forEach(
+      (item) => (calendar[item[0]] = item[1] !== "null" ? item[1] : null)
+    );
+  return calendar;
+};
+
+const listSubDirName = (path: string) => {
+  const list = [];
+
+  for (const item of Deno.readDirSync(path)) {
+    if (item.isDirectory) {
+      list.push(item.name);
+    }
+  }
+
+  return list;
+};
+
+interface BusTimetable {
+  time: string;
+  destination: string;
+  isRushHour: boolean;
+}
+
+interface TrainTimetable {
+  time: string;
+  track: string;
+  destination: string;
+}
+
+const generateBusTimetable = (path: string): BusTimetable[] => {
+  return Deno.readTextFileSync(path)
+    .split("\n")
+    .map((item) => item.split(","))
+    .flatMap((values) => {
+      return {
+        time: values[0],
+        destination: values[1],
+        isRushHour: values[2] === "true",
+      };
+    });
+};
+
+const generateTrainTimetable = (path: string): TrainTimetable[] => {
+  return Deno.readTextFileSync(path)
+    .split("\n")
+    .map((item) => item.split(","))
+    .flatMap((values) => {
+      return {
+        time: values[0],
+        track: values[1],
+        destination: values[2],
+      };
+    });
+};
+
+const generateTimesheet = () => {
+  const timesheet: {
+    [key: string]: {
+      busLeft: BusTimetable[];
+      busRight: BusTimetable[];
+      trainLeft: TrainTimetable[];
+      trainRight: TrainTimetable[];
+    };
+  } = {};
+  listSubDirName("./csv/").forEach((id) => {
+    timesheet[id] = {
+      busLeft: generateBusTimetable(join("csv", id, "bus_left.csv")),
+      busRight: generateBusTimetable(join("csv", id, "bus_right.csv")),
+      trainLeft: generateTrainTimetable(join("csv", id, "train_left.csv")),
+      trainRight: generateTrainTimetable(join("csv", id, "train_right.csv")),
+    };
+  });
+
+  return timesheet;
+};
+
+const busData = JSON.stringify({
+  lastmod: new Date().toISOString(),
+  calendar: generateCalendar(),
+  timesheet: generateTimesheet(),
+});
+
+const encoder = new TextEncoder();
+const contentBytes = encoder.encode(busData);
+Deno.writeFileSync("./csv/busData.json", contentBytes);
+console.log("[Write] ./csv/busData.json");
+
+// deno run --allow-write --allow-read src/generate_json.ts
